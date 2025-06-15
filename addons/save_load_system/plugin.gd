584d59702b544319fc46b70ab7b0c688b2d910a2
NikLucoz <lucozzilavoro@gmail.com>
2025-06-15 14:53:53 +0200

    Fix code duplication error

--- addons/save_load_system/plugin.gd
+++ addons/save_load_system/plugin.gd
@@ -8,19 +8,6 @@ const ENCRYPTION_KEY_SETTING_NAME: String = "save_load_system/encryption_key"
 func _enable_plugin() -> void:
 	if not Engine.has_singleton(AUTOLOAD_NAME):
 		add_autoload_singleton(AUTOLOAD_NAME, "res://addons/save_load_system/save_load_system.gd")
-		SaveLoadSystem._initial_configuration(SAVE_PATH, 1)
-
-func _disable_plugin() -> void:
-	remove_autoload_singleton(AUTOLOAD_NAME)@tool
-
-const AUTOLOAD_NAME = "SaveLoadSystem"
-const USE_ENCRYPTION_SETTING_NAME: String = "save_load_system/use_encryption"
-const ENCRYPTION_KEY_SETTING_NAME: String = "save_load_system/encryption_key"
-const SAVE_PATH: String = "user://saves/"
-
-func _enable_plugin() -> void:
-	if not Engine.has_singleton(AUTOLOAD_NAME):
-		add_autoload_singleton(AUTOLOAD_NAME, "res://addons/save_load_system/save_load_system.gd")
 	
 	var error = DirAccess.make_dir_recursive_absolute(SAVE_PATH) 
 	if error != OK:
@@ -81,29 +68,3 @@ func _exit_tree():
 		dir.remove(file)
 	
 	DirAccess.remove_absolute("user://saves/")
-
-func _enter_tree():
-	if not ProjectSettings.has_setting(USE_ENCRYPTION_SETTING_NAME):
-		ProjectSettings.set_setting(USE_ENCRYPTION_SETTING_NAME, false)
-	
-	ProjectSettings.add_property_info({
-		"name": USE_ENCRYPTION_SETTING_NAME,
-		"type": TYPE_BOOL,
-		"hint": PROPERTY_HINT_FLAGS,
-		"hint_string": "if enable all save files will be encrypted using the provided key"
-	})
-	ProjectSettings.set_initial_value(USE_ENCRYPTION_SETTING_NAME, false)
-	ProjectSettings.set_as_basic(USE_ENCRYPTION_SETTING_NAME, true)
-
-	if not ProjectSettings.has_setting(ENCRYPTION_KEY_SETTING_NAME):
-		ProjectSettings.set_setting(ENCRYPTION_KEY_SETTING_NAME, "un2IAd9ShlecsmvQZVdJsnxya2Abmft3")
-	ProjectSettings.add_property_info({
-		"name": ENCRYPTION_KEY_SETTING_NAME,
-		"type": TYPE_STRING,
-	})
-	ProjectSettings.set_initial_value(ENCRYPTION_KEY_SETTING_NAME, "un2IAd9ShlecsmvQZVdJsnxya2Abmft3")
-	ProjectSettings.set_as_basic(ENCRYPTION_KEY_SETTING_NAME, true)
-	ProjectSettings.save()
-
-func _exit_tree():
-	pass
